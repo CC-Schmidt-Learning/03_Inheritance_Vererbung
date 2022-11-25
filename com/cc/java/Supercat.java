69b3ec5687468f6b07dbeea2221d6a428bc0c507
Daniela <Daniela-Haeussler@gmx.de>
2022-11-25 10:30:00 +0100

    protected

--- com/cc/java/Supercat.java
+++ com/cc/java/Supercat.java
@@ -1,5 +1,24 @@
 package com.cc.java;
 
 public class Supercat {
+    private String name;
+    private String furColor;
+    protected int age;
+
+
+
+
+protected String getStringAttributes(String flag) {
+    switch (flag) {
+        case "#name":
+            return name;
+        case "#color":
+            return furColor;
+        default:
+            return "#!ERROR";
+            }
+        }
     
+      
+
 }
