bfd2057f3314620a6ce7169133ff4bcc72ca9be5
Daniela <Daniela-Haeussler@gmx.de>
2022-11-25 10:32:52 +0100

    return in Tomcat

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -1,30 +1,13 @@
 package com.cc.java;
 
-public class Cat {
- 
-   private String name;
-   private String furColor;
-   private int age;
+public class Cat extends Supercat {
+
 
    private int counter;
 
    public Cat(String name, String furColor, int age) {
-    this.name = name;
-    this.furColor = furColor;
-    this.age = age;
 }
 
-
-    public String getStringAttributes(String flag) {
-        switch (flag) {
-            case "#name":
-                return name;
-            case "#color":
-                return furColor;
-            default:
-                return "#!ERROR";
-        }
-    }
 
     public String getAge() {
             return checkEscalationLevel();
